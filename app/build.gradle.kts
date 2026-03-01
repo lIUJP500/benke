8408df9d6466be76ce189ed37a230f9fbb633e46
锦蓬 刘 <15679675+hhljp813@user.noreply.gitee.com>
2026-03-01 17:49:13 +0800

    智谱sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,4 +102,7 @@ dependencies {
     // HTTP
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    //智谱
+    implementation("ai.z.openapi:zai-sdk:0.3.3")
 }
